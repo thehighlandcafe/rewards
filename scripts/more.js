132abb56df646c237eb0dc1a309afa6a3652b850
aarjay123 <76173037+aarjay123@users.noreply.github.com>
2023-09-27 10:46:23 +0100

    New more activities

--- scripts/more.js
+++ scripts/more.js
@@ -1,15 +1,7 @@
-function showPage(section, moreContainer ) {
-    //hiding more main page
-    document.getElementById(moreContainer).style.display = 'none';
-    
-    //showing more section page that was selected by user
-    document.getElementById(section).style.display = 'block';
+function showPage(activity) {
+    window.location.href = activity;
 }
 
-function backToMain(section, moreContainer) {
-    //hiding more section page that was selected by user
-    document.getElementById(section).style.display = 'none';
-
-    //showing more main page
-    document.getElementById(moreContainer).style.display = 'block';
+function backToMain() {
+    window.location.href = "../index.html";
 }
---
+++ scripts/ripple.js
@@ -0,0 +1,23 @@
+// ripple.js
+document.addEventListener('click', function (e) {
+    const button = e.target.closest('.ripple-button');
+
+    if (button) {
+        const ripple = document.createElement('span');
+        const rect = button.getBoundingClientRect();
+        const size = Math.max(rect.width, rect.height);
+        const x = e.clientX - rect.left - size / 2;
+        const y = e.clientY - rect.top - size / 2;
+
+        ripple.style.width = ripple.style.height = size + 'px';
+        ripple.style.left = x + 'px';
+        ripple.style.top = y + 'px';
+        ripple.classList.add('ripple-effect');
+
+        button.appendChild(ripple);
+
+        setTimeout(() => {
+            ripple.remove();
+        }, 500); // Adjust the time to match your animation duration
+    }
+});
